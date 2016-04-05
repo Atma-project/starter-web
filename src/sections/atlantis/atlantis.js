fc3eb4b48b1ecd6fe7480e2e8ee65b7d2c96fb5e
JeremieDrazic <jerem.drazic@gmail.com>
2016-04-05 23:41:27 +0200

    late commit toniiiiiight  https://github.com/Prism-project/prism-starter-web/blob/master/lolcommits/91b8e20002b.jpg

---
+++ src/sections/atlantis/atlantis.js
@@ -0,0 +1,38 @@
+import './atlantis.scss'
+
+import Vue from 'vue'
+import config from 'config'
+import Experience from './experience/experience.js'
+Vue.config.debug = true
+
+export default Vue.extend({
+    template: require('./atlantis.html'),
+
+    props: [],
+
+    data() {
+        return {
+
+        }
+    },
+
+    events: {
+
+    },
+
+    created() {
+
+    },
+
+    hydrated() {
+
+    },
+
+    watch: {
+
+    },
+
+    methods: {
+
+    }
+})
--- src/sections/index.js
+++ src/sections/index.js
@@ -1,4 +1,5 @@
 import Home from './home/home.js'
 import Map from './map/map.js'
+import Atlantis from './atlantis/atlantis.js'
 
-export default { Home, Map }
+export default { Home, Map, Atlantis }
